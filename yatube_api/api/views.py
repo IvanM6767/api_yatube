732a9dedc9fc52821178d0fd446e44bae37763b7
IvanM6767 <vanioivamami@gmail.com>
2023-04-25 03:06:52 +0300

    reque

---
+++ yatube_api/api/views.py
@@ -0,0 +1,52 @@
+from django.core.exceptions import PermissionDenied
+from posts.models import Comment, Group, Post
+from rest_framework import viewsets
+
+from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(PostViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(PostViewSet, self).perform_destroy(instance)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+
+    def get_queryset(self):
+        new_queryset = Comment.objects.filter(post_id=self.kwargs.get('post'))
+        return new_queryset
+
+    def perform_create(self, serializer):
+        serializer.save(
+            author=self.request.user,
+            post_id=self.kwargs.get('post')
+        )
+
+    def perform_update(self, serializer):
+        if serializer.instance.author != self.request.user:
+            raise PermissionDenied('Изменение чужого контента запрещено!')
+        super(CommentViewSet, self).perform_update(serializer)
+
+    def perform_destroy(self, instance):
+        if instance.author != self.request.user:
+            raise PermissionDenied('Удаление чужого контента запрещено!')
+        super(CommentViewSet, self).perform_destroy(instance)
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -1,17 +1,18 @@
-from django.conf import settings
-from django.conf.urls.static import static
+from api.views import CommentViewSet, GroupViewSet, PostViewSet
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken import views
+
+router_v1 = routers.DefaultRouter()
+router_v1.register(r'groups', GroupViewSet)
+router_v1.register(r'posts', PostViewSet)
+router_v1.register(
+    r'posts/(?P<post>\d+)/comments', CommentViewSet, basename='comment'
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router_v1.urls)),
+    path('api/v1/api-token-auth/', views.obtain_auth_token),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-    )
-    urlpatterns += static(
-        settings.STATIC_URL, document_root=settings.STATIC_ROOT
-    )
