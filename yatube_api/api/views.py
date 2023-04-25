c33c3cf1f295ede67c40eb9412a0e9cf4f6675e6
IvanM6767 <vanioivamami@gmail.com>
2023-04-25 20:31:09 +0300

    requ

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,8 +1,9 @@
 from django.core.exceptions import PermissionDenied
 from posts.models import Comment, Group, Post
 from rest_framework import viewsets
-
+from .permissions import AuthorOrReadOnlyPermissions
 from .serializers import CommentSerializer, GroupSerializer, PostSerializer
+from django.shortcuts import get_object_or_404
 
 
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
@@ -13,6 +14,7 @@ class GroupViewSet(viewsets.ReadOnlyModelViewSet):
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [AuthorOrReadOnlyPermissions]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -30,9 +32,11 @@ class PostViewSet(viewsets.ModelViewSet):
 
 class CommentViewSet(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
+    permission_classes = [AuthorOrReadOnlyPermissions]
 
     def get_queryset(self):
-        new_queryset = Comment.objects.filter(post_id=self.kwargs.get('post'))
+        post = get_object_or_404(Post, id=self.kwargs.get('post'))
+        new_queryset = Comment.objects.filter(post=post)
         return new_queryset
 
     def perform_create(self, serializer):
